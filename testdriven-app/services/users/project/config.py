3c762fafe5e9cdece7eb729a779c6e08d5f33762
alexkobu <alex.kotschi@gmail.com>
2019-09-28 00:17:32 +0200

    tests added

--- testdriven-app/services/users/project/config.py
+++ testdriven-app/services/users/project/config.py
@@ -5,18 +5,19 @@ import os
 class BaseConfig:
     """Base configuration"""
     TESTING = False
-    SQLALCHEMY_TRACK_MODIFICATION = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'my_precious'
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_ULR')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class TestingConfig(BaseConfig):
     """Testing configuration"""
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_ULR')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 
-class ProductonConfig(BaseConfig):
+class ProductionConfig(BaseConfig):
     """Production configuration"""
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_ULR')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
